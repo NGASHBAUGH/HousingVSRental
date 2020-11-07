46895b027046dcca775c66e2bd60e2466fe517cc
Michael Munson <Muntz_99@outlook.com>
2020-11-07 14:15:52 -0800

    Update app.py
    Building shell

--- assets/app.py
+++ assets/app.py
@@ -4,6 +4,27 @@ import requests
 import pandas as pd
 import os 
 import numpy as np
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
+from flask import Flask
+
+engine = create_engine('postgresql://scott:tiger@localhost/mydatabase')
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+
+session = Session(engine)
 
 app = Flask(__name__)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
