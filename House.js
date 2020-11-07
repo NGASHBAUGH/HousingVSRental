8d4f221adea641076783f0150796e42cab7b9bcb
Nathan Ashbaugh <ngashbaugh@gmail.com>
2020-11-07 10:22:17 -0800

    Nathan

---
+++ House.js
@@ -0,0 +1,25 @@
+apiKey = "76d2396840mshc562c26618d33a2p1fb1e6jsn95b66aa3ea3c";
+
+// d3.json(`https://www.quandl.com/api/v3/datasets/ZILLOW/C19607_ZHVISF?api_key=sPG_jsHhtuegYcT7TNWz`).then(function (data){
+//     console.log("Salt Lake")    
+//     console.log(data)
+// });
+
+
+
+d3.json(`https://www.quandl.com/api/v3/datasets/ZILLOW/C19496_ZHVISF?api_key=sPG_jsHhtuegYcT7TNWz`).then(function (data){
+    console.log(data)
+});
+
+
+function area (area) {
+    var values = {'State' : "S" , "Country" : "CO" , "City" : "C" , "Zipcode" : "Z"}
+    ul = d3.select('#sample-metadata').html('')
+    Object.entries()
+}
+
+
+function optionChanged () {
+    id = d3.select('#selDataset').property('value');
+    return id
+};
