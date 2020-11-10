3b7c7c42adc3c1698e9e2343de8f388147406680
Michael Munson <Muntz_99@outlook.com>
2020-11-09 17:32:22 -0800

    dependencies updated

--- assets/app.py
+++ assets/app.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 from splinter import Browser
+from flask import Flask, jsonify, render_template, redirect, url_for, Response
 import requests
 import pandas as pd
 import os 
 import numpy as np
 from sqlalchemy import create_engine
+import json
 
 app = Flask(__name__)
+
