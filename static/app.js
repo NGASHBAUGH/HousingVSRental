558cd08fa7431093d3f6b156d5c5f0ab14456444
Jordan Usner <jordan.usner@gmail.com>
2020-11-14 10:46:56 -0800

    changes

--- static/app.js
+++ static/app.js
@@ -33,6 +33,7 @@ function handleSubmit(){
     // console.log(userInput)
     d3.select('#input').node().value = "";
     apiCall(userInput)
+    createMap()
 }
 
 function apiCall(input) {
@@ -123,7 +124,45 @@ function category () {
     return areaCategory
 }
 
+//MAP
+function createMap() {
 
+    d3.csv("uszipcodes.csv").then(function(data) {
+        console.log(data[0]);
+      });
+
+    // Define streetmap and darkmap layers
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "map",
+      accessToken: "pk.eyJ1Ijoiam9yZGFudSIsImEiOiJja2dyYjN0MGowMHZ5MnVuenR0ZG8weHl2In0.wlCzpVVa_PgxzWRK2s2rRA"
+    });
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "Street Map": streetmap,
+    };
+
+    var myMap = L.map("map", {
+        center: [
+          37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [streetmap]
+      });
+    
+  
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  }
+  
 
 
 // this is linked to the submit button on the page and activates the search 
